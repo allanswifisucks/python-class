39eef63960cddad97667fb87e8d6bae0549bfbe5
allanswifisucks <157652618+allanswifisucks@users.noreply.github.com>
2024-09-19 20:23:45 -0400

    Add files via upload
    this is my code

---
+++ sept19.py
@@ -0,0 +1,14 @@
+while True:
+    num1=int(input("num1\n"))
+    op=input("op\n")
+    num2=int(input("num2\n"))
+    if op=="+":
+        print(num1+num2)
+    if op=="-":
+        print(num1-num2)
+    if op=="*":
+        print(num1+num2)
+    if op=="/":
+        print(num1+num2)
+    if op=="break":
+        break
