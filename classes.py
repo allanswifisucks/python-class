9366c23be5483a4a8afbe0d7e1daee371915e5cb
allanswifisucks <157652618+allanswifisucks@users.noreply.github.com>
2024-09-26 20:23:44 -0400

    Add files via upload
    Description of Jabyou

---
+++ classes.py
@@ -0,0 +1,16 @@
+#class for jasper
+class jasper:
+    name = "jasper"
+    favphrase = "You are really silly!"
+    lastname = "liem"
+    mittlename = "???"
+
+#object jasper
+
+
+jasperrr = jasper()
+print(jasperrr.name)
+print(jasperrr.favphrase)
+print(jasperrr.lastname)
+print(jasperrr.mittlename)
+
