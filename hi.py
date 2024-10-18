aa2e9991cfb2d0756c6dbf9d40cae1c748838b70
allanswifisucks <157652618+allanswifisucks@users.noreply.github.com>
2024-10-17 20:34:39 -0400

    Add files via upload
    book code

---
+++ hi.py
@@ -0,0 +1,33 @@
+class book:
+    def __init__(self, title, author, genre, year_published):
+        self.title = title
+        self.author = author
+        self.genre = genre
+        self.year_published = year_published
+joshsbook = book("the ghaleb😈","ghaleb😈👿🙀","horor😈😵","???😨")
+print("ghalebs book")
+print(joshsbook.title)
+print(joshsbook.author)
+print(joshsbook.genre)
+print(joshsbook.year_published)
+print("💩💩💩💩💩💩💩💩💩💩💩💩💩💩💩💩💩💩💩💩💩💩💩💩💩💩💩💩💩💩💩💩💩💩")
+title = input("whats the title?\n")
+author = input("whats the author?\n")
+genre = input("whats the genre?\n")
+year_published = input("whats the wwip?\n")
+
+print(f"\n\ninfo about your book\n{title}\n{author}\n{genre}\n{year_published}")
+    # title = "theghaleb"
+    # author = "ghaleb"
+    # genrere = "horor"
+    # year_published = "2024"
+    
+    # title, author, genre, and year_published.
+# joshsbook = book()
+# print(joshsbook.title)
+# print(joshsbook.author)
+# print(joshsbook.genrere)
+# print(joshsbook.year_published)
+
+# def __init__(self, title, author, genre, year_published):
+#         self.title = title
