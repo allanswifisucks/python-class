184e4c0c849649fa3b040174e49cd2715b4e020e
allanswifisucks <157652618+allanswifisucks@users.noreply.github.com>
2024-11-14 20:18:21 -0500

    Add files via upload

--- q.py
+++ q.py
@@ -51,7 +51,7 @@ def addexpence():
         description=input("whats the description\n")
         date=input("whats the whats the date\n")
         location=input("whats the location\n")
-        
+#this asks for the expence and stuff 
 
         expencedict= {"expence":expence,"cost":cost,"description":description,"date":date,"location":location}
 
@@ -64,21 +64,53 @@ def addexpence():
             print("ok")
         if con==("no"):
             break
+#this asks if you want to continue
+def vebc():
+    print("the catergorys are\n")
+    for g in expencelist:
+    
+        print(g["expence"])
+    
+    categorychoice=input("chose a category")
+    for h in expencelist:
+        if categorychoice==h["expence"]:
+            print(h)
+        con1=input("do you want to continue\n")
+        if con1==("yes"):
+            print("ok")
+        if con1==("no"):
+            break
+def calculatetotalexpenses():
+    costtotal=0
 
-
-
-
+    for f in expencelist:
+        costtotal+=f["cost"]
+    print(f"the total is\n{costtotal}")   
+    
 
 def viewexpenses():
     for I in expencelist:
         print(I)
-print("1.add expence\n2.veiw expences\n3.quit")
+#This makes a for loop       
+
 while True:
-   
+    print("1.add expence\n2.veiw expences\n3.calculate total expenses\n4.view expenses by catergory\n5.save expence\n6.Qwit")
+# This shows the menu 
     menu=input("Choose an option\n")
-    if menu==("1"):
+    if menu=="1":
         addexpence()
-    if menu==("2"):
+    if menu=="2":
         viewexpenses()
-    if menu==("3"):
+    if menu=="3":
+        calculatetotalexpenses()
+    if menu=="4":
+        vebc()    
+    if menu=="5":
+        continue    
+    if menu=="6":
         break
+#you chose a menu opption 
+
+# 3. Calculate Total Expenses
+# 4. View Expenses by Category
+# 5. Save Expenses
